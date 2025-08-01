843f5da239c325680d3d6759247b41aff18cd830
mychemicalcode <jty7027@gmail.com>
2025-08-01 13:23:24 +0900

    first commit

---
+++ src/main/java/com/study/rz001webgametrpgv2/Rz001WebgameTrpgV2Application.java
@@ -0,0 +1,13 @@
+package com.study.rz001webgametrpgv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rz001WebgameTrpgV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rz001WebgameTrpgV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/rz001webgametrpgv2/Rz001WebgameTrpgV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.rz001webgametrpgv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rz001WebgameTrpgV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
